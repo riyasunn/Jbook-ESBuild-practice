fba6177e2116b34d43beb93b3b1134a877f80681
Riya Sun <riya.sunn@gmail.com>
2023-08-11 16:53:33 +1200

    add esbuil and transpil jsx to es2015, display the transpiled code

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,43 @@
+import * as esbuild from 'esbuild-wasm';
 import ReactDOM from "react-dom";
+import { useState, useEffect, useRef } from "react";
+
 
 const App = () => {
-  return <h1>HI</h1>
+  const [ input, setInput ] = useState('');
+  const [ code, setCode ] = useState('');
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async() => {
+    if(!ref.current){
+      return
+    };
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+    console.log(result);
+    setCode(result.code);
+  };
+
+  return <div>
+    <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+    <div>
+      <button onClick={onClick}>Submit</button>
+    </div>
+    <pre>{code}</pre>
+  </div>;
 };
 
 ReactDOM.render(
